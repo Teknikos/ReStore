0cd212caf95736a540a9f220c8d462b70040d438
Patrik Morell <morellpatrik@gmail.com>
2022-11-07 02:29:02 +0100

    Added Mui

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    length: number
+    id: number,
+    name: string
+    description: string
+    price: number
+    pictureUrl: string
+    type?: string
+    brand: string
+    quantityInStock: number
+}
